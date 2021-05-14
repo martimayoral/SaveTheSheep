f711f7f914d3a5b52f267ca7a2c3ce467f5e3bfa
martimayoral <76410086+martimayoral@users.noreply.github.com>
2021-05-14 15:22:09 +0200

    added Sheep as Dogs

---
+++ Assets/scripts/Dog.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour
+{
+    public GameObject player;
+    public float interpolationFactior = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.SetPositionAndRotation(player.transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform.position);
+        transform.position += (player.transform.position - transform.position) * interpolationFactior;
+    }
+}
