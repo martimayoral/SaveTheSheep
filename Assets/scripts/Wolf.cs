85a03514fc2254420b97bfa1b26b9942b682968e
danicharles99 <56824248+danicharles@users.noreply.github.com>
2021-05-26 17:59:25 +0200

    Basic wolf movement

---
+++ Assets/scripts/Wolf.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wolf : MonoBehaviour
+{
+    [SerializeField] [Range(0.0f, 4.0f)] float wolfLerpTime;
+    [SerializeField] Vector3[] wolfPositions;
+
+    private int indexPos = 0;
+
+    private int numberOfPositions;
+
+    private float time = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberOfPositions = wolfPositions.Length;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, wolfPositions[indexPos], wolfLerpTime * Time.deltaTime);
+
+        time = Mathf.Lerp(time, 1.0f, wolfLerpTime * Time.deltaTime);
+        if (time > .9f)
+        {
+            time = 0.0f;
+            indexPos = (indexPos + 1) % numberOfPositions;
+        }
+    }
+}
