1685dfdb2afdd18586864bed5af088ef8d2ee6c6
danicharles99 <56824248+danicharles@users.noreply.github.com>
2021-05-26 18:57:00 +0200

    Interpolar la salida del lobo

--- Assets/scripts/Wolf.cs
+++ Assets/scripts/Wolf.cs
@@ -6,22 +6,33 @@ public class Wolf : MonoBehaviour
 {
 
     private float timerCounter = 0;
+    private bool wolfInScene;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        wolfInScene = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timerCounter += Time.deltaTime * 0.5f;
+        if(wolfInScene)
+        { 
+            timerCounter += Time.deltaTime * 0.5f;
 
-        float x = 50 + Mathf.Cos(timerCounter) * 35;
-        float y = 0;
-        float z = 37 + Mathf.Sin(timerCounter) * 20;
+            float x = 50 + Mathf.Cos(timerCounter) * 35;
+            float y = 0;
+            float z = 37 + Mathf.Sin(timerCounter) * 20;
 
-        transform.position = new Vector3(x, y, z);
+            Vector3 nextPoint = new Vector3(x, y, z);
+
+            transform.position = Vector3.Lerp(transform.position, nextPoint, Time.deltaTime);
+
+        } else
+        {
+            timerCounter += Time.deltaTime * 0.3f;
+            if (timerCounter > 1.0f) wolfInScene = true;
+        }
     }
 }
