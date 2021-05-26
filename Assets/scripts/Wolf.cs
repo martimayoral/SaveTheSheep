5ee030c2f023995b3d4a9692ee1e6f31e2cd2c54
danicharles99 <56824248+danicharles@users.noreply.github.com>
2021-05-26 18:25:23 +0200

    wolf look at point where he go

--- Assets/scripts/Wolf.cs
+++ Assets/scripts/Wolf.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Wolf : MonoBehaviour
 {
-    [SerializeField] [Range(0.0f, 4.0f)] float wolfLerpTime;
+    [SerializeField] [Range(0.0f, 4.0f)] float wolfSpeed;
     [SerializeField] Vector3[] wolfPositions;
 
     private int indexPos = 0;
@@ -12,21 +12,27 @@ public class Wolf : MonoBehaviour
     private int numberOfPositions;
 
     private float time = 0.0f;
+    Rigidbody rigidbody;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        numberOfPositions = wolfPositions.Length;
+        rigidbody = GetComponent<Rigidbody>();
 
+        numberOfPositions = wolfPositions.Length;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, wolfPositions[indexPos], wolfLerpTime * Time.deltaTime);
+        Vector3 toLook = wolfPositions[indexPos];
+        transform.position = Vector3.Lerp(transform.position, wolfPositions[indexPos], wolfSpeed * Time.deltaTime);
 
-        time = Mathf.Lerp(time, 1.0f, wolfLerpTime * Time.deltaTime);
+        transform.LookAt(new Vector3(toLook.x, 0, toLook.z));
+
+        time = Mathf.Lerp(time, 1.0f, wolfSpeed * Time.deltaTime);
         if (time > .9f)
         {
             time = 0.0f;
