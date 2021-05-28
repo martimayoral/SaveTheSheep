0deaf4589612c7ad75c8d0f4f3c018792ad0c9b8
martimayoral <76410086+martimayoral@users.noreply.github.com>
2021-05-28 11:27:54 +0200

    wolf hunt sheep

--- Assets/scripts/Sheep.cs
+++ Assets/scripts/Sheep.cs
@@ -7,6 +7,7 @@ public class Sheep : MonoBehaviour
     public float movementSpeed;
     public float rotationSpeed;
     private List<GameObject> players = new List<GameObject>(); // 5
+    private GameObject wolf; // 5
     public float playerRepeelDistance;
     Rigidbody rigidbody;
 
@@ -19,6 +20,8 @@ public class Sheep : MonoBehaviour
         rigidbody = GetComponent<Rigidbody>();
         players.Add(GameObject.Find("Red Player"));
         players.Add(GameObject.Find("Blue Player"));
+
+        wolf = GameObject.Find("Wolf");
     }
 
     void move(Vector3 desiredPosition)
@@ -32,6 +35,31 @@ public class Sheep : MonoBehaviour
             rigidbody.MovePosition(hit.point);
     }
 
+    Vector3 repeel(GameObject player, float distance)
+    {
+        Vector3 toLook = Vector3.zero;
+
+        // vector from player and sheep
+        Vector3 vec = player.transform.position - this.transform.position;
+
+        // direction: vector normalized
+        Vector3 direction = -vec.normalized;
+
+        // distance to move
+        float dist = playerRepeelDistance - vec.magnitude;
+
+        // if the distance to move is away from the player
+        if (dist > 0)
+        {
+            // translation to aply
+            toLook += movementSpeed * direction * Time.deltaTime * dist * -1;
+            rigidbody.AddForce(movementSpeed * direction * dist);
+        }
+
+        return toLook;
+    }
+
+
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -39,23 +67,9 @@ public class Sheep : MonoBehaviour
         Vector3 toLook =  this.transform.forward * 10;
 
         foreach (GameObject player in players ){
-            // vector from player and sheep
-            Vector3 vec = player.transform.position - this.transform.position;
-
-            // direction: vector normalized
-            Vector3 direction = -vec.normalized;
-
-            // distance to move
-            float dist = playerRepeelDistance - vec.magnitude;
-
-            // if the distance to move is away from the player
-            if (dist > 0)
-            {
-                // translation to aply
-                toLook += movementSpeed * direction * Time.deltaTime * dist * -1;
-                rigidbody.AddForce(movementSpeed * direction * dist);
-            }
+            toLook += repeel(player, playerRepeelDistance);
         }
+        toLook += repeel(wolf, playerRepeelDistance * 0.5f);
 
         // update the rotation. Slerp will go to the desired location smoothly
         rigidbody.MoveRotation(
--- Assets/scripts/Wolf.cs
+++ Assets/scripts/Wolf.cs
@@ -4,37 +4,79 @@ using UnityEngine;
 
 public class Wolf : MonoBehaviour
 {
+    private enum State
+    {
+        outside,
+        inside,
+        hunting,
+        leaving
+    }
+    private State state;
 
-    private float timerCounter = 0;
-    private bool wolfInScene;
+    private float toInside;
+    private float toHunt;
+
+    public GameObject Sheeps;
+
+    private void reset()
+    {
+        toInside = Time.time + Random.Range(3,5) ;
+        toHunt = toInside + Random.Range(10, 15);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        wolfInScene = false;
+        state = State.outside;
+        reset();
+    }
+
+    void stateOutside()
+    {
+        if (Time.time > toInside) state = State.inside;
+    }
+
+    void stateInside()
+    {
+        float x = 50 + Mathf.Cos(Time.time * 0.5f) * 35;
+        float y = 0;
+        float z = 37 + Mathf.Sin(Time.time * 0.5f) * 20;
+
+        Vector3 nextPoint = new Vector3(x, y, z);
+
+        transform.LookAt(nextPoint);
+
+        transform.position = Vector3.Lerp(transform.position, nextPoint, Time.deltaTime);
+
+        if (Time.time > toHunt) state = State.hunting;
+    }
+
+    void stateHunting()
+    {
+        GameObject sheep;
+        sheep = Sheeps.transform.GetChild(Random.Range(0,Sheeps.transform.childCount-1)).gameObject;
+
+        sheep.transform.position = new Vector3(50, 50, 50);
+
+        state = State.leaving;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(wolfInScene)
-        { 
-            timerCounter += Time.deltaTime * 0.5f;
-
-            float x = 50 + Mathf.Cos(timerCounter) * 35;
-            float y = 0;
-            float z = 37 + Mathf.Sin(timerCounter) * 20;
-
-            Vector3 nextPoint = new Vector3(x, y, z);
-
-            transform.LookAt(nextPoint);
-
-            transform.position = Vector3.Lerp(transform.position, nextPoint, Time.deltaTime);
-
-        } else
+        switch (state)
         {
-            timerCounter += Time.deltaTime * 0.3f;
-            if (timerCounter > 1.0f) wolfInScene = true;
+            case State.outside:
+                stateOutside();
+                break;
+            case State.inside:
+                stateInside();
+                break;
+            case State.hunting:
+                stateHunting();
+                break;
+            case State.leaving:
+                break;
         }
     }
 }
